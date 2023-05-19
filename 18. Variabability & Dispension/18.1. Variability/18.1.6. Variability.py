d4df9c269b5929dc966858a48c5d794b1a48ad22
Seng Keat <corodostudio@gmail.com>
2023-05-20 01:15:16 +0700

    Create 18.1.6. Variability.py

---
+++ 18. Variabability & Dispension/18.1. Variability/18.1.6. Variability.py
@@ -0,0 +1,28 @@
+import statistics
+
+# Define the data
+data = [1, 2, 3, 4, 5]
+
+# Calculate the mean
+mean = statistics.mean(data)
+
+# Calculate the variance
+variance = statistics.variance(data)
+
+# Calculate the standard deviation
+standard_deviation = statistics.standard_deviation(data)
+
+# Print the results
+print("Mean:", mean)
+print("Variance:", variance)
+print("Standard deviation:", standard_deviation)
+
+# This code will print the following output:
+#Mean: 3
+#Variance: 2
+#Standard deviation: 1.4142135623730951
+
+# The mean is the average of the data. The variance is the average squared deviation of the data from the mean. The standard deviation is the square root of the variance.
+
+#These measures of variability can be used to understand how spread out the data is. A high variance indicates that the data is spread out, while a low variance indicates that the data is clustered together.  
+
