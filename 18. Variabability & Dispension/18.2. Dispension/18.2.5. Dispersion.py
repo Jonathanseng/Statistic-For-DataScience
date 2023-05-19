5c18031457b3479f3e3722c00da970cc330c23a5
Seng Keat <corodostudio@gmail.com>
2023-05-20 01:02:25 +0700

    Create 18.2.5. Dispersion.py

---
+++ 18. Variabability & Dispension/18.2. Dispension/18.2.5. Dispersion.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+# Define the data
+data = [1, 2, 3, 4, 5]
+
+# Calculate the mean
+mean = np.mean(data)
+
+# Calculate the variance
+variance = np.var(data)
+
+# Calculate the standard deviation
+standard_deviation = np.std(data)
+
+# Print the results
+print("Mean:", mean)
+print("Variance:", variance)
+print("Standard deviation:", standard_deviation)
+
+# This code will print the following output:
+Mean: 3
+Variance: 2
+Standard deviation: 1.4142135623730951
+
+ # Here is a brief explanation of each measure of dispersion:
+
+#Mean: The mean is the average of the data points. It is calculated by adding all of the data points and then dividing by the number of data points.
+#Variance: The variance is a measure of how spread out the data points are from the mean. It is calculated by taking the average of the squared differences between the data points and the mean.
+#Standard deviation: The standard deviation is the square root of the variance. It is a measure of how far, on average, the data points are from the mean.
+#These measures of dispersion can be used to compare different data sets or to track how a data set changes over time. 
