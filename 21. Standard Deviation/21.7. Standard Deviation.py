aeaafef07e0b933c998353c593cc6c0ca48db357
Seng Keat <corodostudio@gmail.com>
2023-05-20 14:16:56 +0700

    Create 21.7. Standard Deviation.py

---
+++ 21. Standard Deviation/21.7. Standard Deviation.py
@@ -0,0 +1,23 @@
+import statistics
+
+# Define the data set
+data = [1, 2, 3, 4, 5]
+
+# Calculate the mean
+mean = statistics.mean(data)
+
+# Calculate the variance
+variance = statistics.variance(data)
+
+# Calculate the standard deviation
+standard_deviation = statistics.stdev(data)
+
+# Print the results
+print("Mean:", mean)
+print("Variance:", variance)
+print("Standard deviation:", standard_deviation)
+
+# his code will print the following output:
+Mean: 3
+Variance: 2.5
+Standard deviation: 1.5811388300841898
