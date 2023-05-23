4a5505eb6ec5ddb7aa7b09550853fbdbc93340da
Seng Keat <corodostudio@gmail.com>
2023-05-23 21:11:36 +0700

    Create 5.5. Element.py

---
+++ 5. Element/5.5. Element.py
@@ -0,0 +1,23 @@
+import statistics
+
+# Create a list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Find the mode of the list
+mode = statistics.mode(numbers)
+
+# Print the mode
+print(mode)
+
+# This code will print the following output:
+3
+
+# The mode of the list is 3 because it appears the most times in the list.
+
+#Here are some other statistical functions that you can use in Python:
+
+#mean() - Calculates the mean of a list of numbers.
+#median() - Calculates the median of a list of numbers.
+#standard_deviation() - Calculates the standard deviation of a list of numbers.
+#variance() - Calculates the variance of a list of numbers.
+#You can learn more about these functions in the Python documentation: https://docs.python.org/3/library/statistics.html
